3e2cc4bc5d9d5294ed844b6864999d559d139a69
Irvin Moraga <irvinmoraga@gmail.com>
2021-10-19 21:44:41 +0800

    initial commit

---
+++ getIPV4-App.py
@@ -0,0 +1,21 @@
+import requests
+
+print("==================== GET THE PUBLIC IP ADDRESS OF THE DEVICE ====================")
+ip = requests.get("https://api.ipify.org").content.decode("utf8")
+
+api_url = "https://ipapi.co/json"
+response = requests.get(api_url)
+api_response = response.json()
+
+ip = api_response["ip"]
+city = api_response["city"]
+region = api_response["region"]
+country = api_response["country_name"]
+asn = api_response["asn"]
+org = api_response["org"]
+
+print("The public IP address of your system is: " + ip)
+print("You are in estimated to be in " + city + " which is located in " + region + ", " + country)
+print("Your internet service provider is " + org + ". The ASN of your provider is " + asn)
+
+
